721d81eb246eb867fc592ff27221e87b3a11e280
Jigar <shahjigar185@gmail.com>
2021-04-03 23:58:15 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Route,Switch} from 'react-router-dom';
+import Landing from './pages/Landing';
+import Filters from './components/Filters';
+import './App.css';
+import './bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+         <h1 className="text-center">SpaceX launch Programs</h1>
+         <Route exact path ='/' component={Landing} />
+    </React.Fragment>
   );
 }
 
